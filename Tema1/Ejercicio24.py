7d139ae79d82a9a2a06230586ade27cb85e3aa36
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-10-08 10:19:28 +0200

    Update Ejercicio24.py

--- Tema1/Ejercicio24.py
+++ Tema1/Ejercicio24.py
@@ -1,10 +1,8 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> suma = 0
-... numero = float(input("Introduce un número (0 para terminar): "))
-... 
-... while numero != 0:
-...     suma += numero
-...     numero = float(input("Introduce otro número (0 para terminar): "))
-... 
-... print("La suma total es:", suma)
+suma = 0
+numero = float(input("Introduce un número (0 para terminar): "))
+ 
+while numero != 0:
+  suma += numero
+  numero = float(input("Introduce otro número (0 para terminar): "))
+  print("La suma total es:", suma)
+
