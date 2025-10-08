9c62f83115c764e14da004b46bf661390585f4b2
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-10-08 12:53:52 +0200

    subida de ejercicios

---
+++ Tema1/Ejercicio26.py
@@ -0,0 +1,4 @@
+for i in range(1, 101):
+    suma += i
+
+print("La suma del 1 al 100 es:", suma
---
+++ Tema1/Ejercicio27.py
@@ -0,0 +1,5 @@
+suma=0
+for i in range(100, 0, -1):
+    suma += i
+
+print("La suma del 1 al 100 es:", suma)
