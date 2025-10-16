6f03174f026f5a0ef05c606c6e8016cdb6f3861f
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-10-16 10:29:52 +0200

    subida

--- Tema1/Ejercicio35.py
+++ Tema1/Ejercicio35.py
@@ -2,10 +2,22 @@
 # nombre de usuario pida una contraseña y valide que
 # la contraseña tenga al menos 8 caracteres.
 
+#usuario = input("Introduce tu nombre de usuario: ")
+#contraseña = input("Introduce tu contraseña: ")
+
+#if len(contraseña) < 8:
+#    print("Error: la contraseña debe tener al menos 8 caracteres.")
+#else:
+#    print("Login correcto. ¡Bienvenido,", usuario + "!")
+
+
+# Login con validación de contraseña y reintento
+
 usuario = input("Introduce tu nombre de usuario: ")
 contraseña = input("Introduce tu contraseña: ")
 
-if len(contraseña) < 8:
-    print("Error: la contraseña debe tener al menos 8 caracteres.")
-else:
-    print("Login correcto. ¡Bienvenido,", usuario + "!")
+while len(contraseña) < 8:
+    print("Error: la contraseña debe tener al menos 8 caracteres")
+    contraseña = input("Vuelve a introducir tu contraseña: ")
+
+print("Login correcto. ¡Bienvenido,", usuario)
