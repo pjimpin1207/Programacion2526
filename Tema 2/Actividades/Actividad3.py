622a10b8df411735efdf0e084aeadbb8a7b23f37
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-10-29 12:41:38 +0100

    subida de ejercicios

---
+++ Tema 2/Actividades/Actividad3.py
@@ -0,0 +1,9 @@
+a = [1,2,3,4]
+for i in range (3):
+    print (a[i])
+
+
+# Lo corregiria así
+
+for i in range(len(a)):
+    print (a[i])
---
+++ Tema 2/Actividades/Actividad4.py
@@ -0,0 +1,8 @@
+sueldos = [1800, 1200, 2000, 1200, 900]
+
+for i in range(len(sueldos)):
+    sueldos[i] = sueldos[i] * 1.10
+    print(sueldos)
+    
+
+    
