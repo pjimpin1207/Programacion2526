5ad62d707e0b0ad0795f3327bb2abe4a21c0c4d8
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-11-05 10:45:20 +0100

    subida de ejercicios

--- Tema 2/Actividades/Actividad9.py
+++ Tema 2/Actividades/Actividad9.py
@@ -1,14 +1,13 @@
 alumnos = ["Pablo", "David", "Alvaro", "Adrian"]
 
-
-# Mostrar el array inicial
+# array inicial
 print("Lista inicial de alumnos:", alumnos)
 
-# Pedir un nuevo alumno al usuario
-nuevo_alumno = input("Introduce el nombre de un nuevo alumno: ")
+# Pedir un nuevo alumno
+nuevo_alumno = input("Introduce un nuevo alumno: ")
 
 # Añadirlo al final de la lista
 alumnos.append(nuevo_alumno)
 
 # Mostrar el resultado
-print("Lista actualizada de alumnos:", alumnos)
+print("Lista final de alumnos:", alumnos)
