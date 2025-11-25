8ce8fb104e51def8f3f92ce300cba908bb5d8966
pjimpin1207 <pjimpin1207@g.educaand.es>
2025-11-25 14:56:47 +0100

    subida de ejercicios

---
+++ Temas/Tema2/EjerciciosRefuerzo/Ejercicio1.py
@@ -0,0 +1,7 @@
+inventarioVacio = []
+print(f"Inventario vacío: {inventarioVacio}")
+
+diasSemana = ["Lunes", "Martes", "Miércoles"]
+print(f"Días de la semana: {diasSemana}")
+puntuacionesTrimestre = [85, 90, 78, 95]
+print(f"Puntuaciones: {puntuacionesTrimestre}")
---
+++ Temas/Tema2/EjerciciosRefuerzo/Ejercicio2.py
@@ -0,0 +1,12 @@
+ciudades =  ["Madrid", "Barcelona", "Valencia", "Sevilla", "Bilbao"]
+
+primeraCiudad = ciudades[0]
+print (primeraCiudad)
+
+ultimaCiudad = ciudades[-1]
+print (ultimaCiudad)
+
+ciudades [3] = "Malaga"
+print (ciudades[3])
+
+print (ciudades)
