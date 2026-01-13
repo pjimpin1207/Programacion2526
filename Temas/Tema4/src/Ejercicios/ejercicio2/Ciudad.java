79cd26a30f19a351954efdfbdc0efbe1be29aae8
pjimpin1207 <pjimpin1207@g.educaand.es>
2026-01-13 14:56:19 +0100

    terminar ejercicio1

--- Temas/Tema4/src/Ejercicios/ejercicio2/Ciudad.java
+++ Temas/Tema4/src/Ejercicios/ejercicio2/Ciudad.java
@@ -4,17 +4,30 @@ public class Ciudad {
 
     public static void main(String[] args) {
 
-        Persona persona1 = new Persona("Pablo", 21);
+        Persona persona1 = new Persona("Pablo", 21, 1.80f);
         persona1.nombre = "Pablo";
         persona1.edad = 21;
 
-        Persona persona2 = new Persona("Luis", 22);
-        persona2.nombre = "Luis";
+        Persona persona2 = new Persona("Francisco", 22, 1.60f);
+        persona2.nombre = "Francisco";
         persona2.edad = 22;
-        
-        Persona persona3 = new Persona("Javier", 20);
+
+        Persona persona3 = new Persona("Javier", 20, 1.90f);
         persona3.nombre = "Javier";
         persona3.edad = 20;
+
+        Persona pepe = new Persona();
+        Persona paco = new Persona();
+
+        Persona ana = new Persona("Ana", 25, 1.65f);
+        Persona luis = new Persona("Luis", 30, 1.80f);
+
+        Persona juan = new Persona("Juan", "Pérez", 22, 1.75f);
+
+        System.out.println(juan);
+
+        System.out.println(pepe);
+        System.out.println(paco);
 
         System.out.println(persona1);
         System.out.println(persona2);
--- Temas/Tema4/src/Ejercicios/ejercicio2/Persona.java
+++ Temas/Tema4/src/Ejercicios/ejercicio2/Persona.java
@@ -1,16 +1,40 @@
 package Ejercicios.ejercicio2;
 
 public class Persona {
+
     String nombre;
     int edad;
+    float estatura;
 
-    public Persona(String nombre, int edad) {
+    // Constructor sin parámetros
+    public Persona() {
+        nombre = "anónimo";
+        edad = 18;
+        estatura = 1.70f;
+    }
+
+    // Constructor que incluye solo el nombre
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        edad = 18;
+        estatura = 1.70f;
+    }
+
+    // Constructor con todos los parámetros
+    public Persona(String nombre, int edad, float estatura) {
         this.nombre = nombre;
         this.edad = edad;
+        this.estatura = estatura;
     }
+
+    //Constructor con el apellido
+    public Persona(String nombre, String apellido, int edad, float estatura) {
+        this(nombre + " " + apellido, edad, estatura);
+    }
+
 
     @Override
     public String toString() {
-        return "Nombre = " + nombre + " Edad = " + edad;
+        return "Persona: Nombre = " + nombre + ", edad = " + edad + ", estatura = " + estatura + "]";
     }
 }
