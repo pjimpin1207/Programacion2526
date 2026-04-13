4401946dd8f4153b832fae55fd8071746bb59305
pjimpin1207 <pjimpin1207@g.educaand.es>
2026-04-13 17:52:03 +0200

    subir ejercicio5

--- ProyecoIntellij/Programacion2526/src/Tema8/Ejercicios/ejercicio5/Ejercicio5.java
+++ ProyecoIntellij/Programacion2526/src/Tema8/Ejercicios/ejercicio5/Ejercicio5.java
@@ -1,6 +1,8 @@
 package Tema8.Ejercicios.ejercicio5;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Ejercicio5 {
@@ -28,7 +30,20 @@ public class Ejercicio5 {
         numerosPrimos.add(2);
         numerosPrimos.add(3);
 
-        
+        // El método containsAll() mira si todos los elementos de "numerosPrimos" están en "numeros"
+        boolean esSubconjunto = numeros.containsAll(numerosPrimos);
+        System.out.println("¿Es {2,3} un subconjunto?: " + esSubconjunto);
 
+        //Lista final con los valores del conjunto
+        numeros.remove(null);
+        final List<Integer> listaFinal = new ArrayList<>(numeros);
+        listaFinal.add(100);
+        System.out.println("Lista final (con modificador final): " + listaFinal);
     }
 }
+
+
+        /*
+           - ¿Es la lista inmutable? no el objeto sigue siendo mutable
+           - ¿Podemos añadir/borrar? si porque final solo impide reasignar la variable
+        */
