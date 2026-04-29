8989b128ec88074cd7891abf5b211014990a96ca
pjimpin1207 <pjimpin1207@g.educaand.es>
2026-04-29 11:15:26 +0200

    añadir ejercicios al porfolio y crear tema 9

---
+++ ProyecoIntellij/Programacion2526/src/Tema9/Ejemplos/EjemploIO.java
@@ -0,0 +1,13 @@
+package Tema9.Ejemplos;
+
+import java.util.Scanner;
+
+public class EjemploIO {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nombre = sc.nextLine();
+        System.out.println("Me llamo: " + nombre);
+    }
+
+
+}
